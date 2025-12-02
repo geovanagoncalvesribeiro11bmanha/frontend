861277e9744839395b937b3689564592984e223b
EA - Geovana Gonçalves Ribeiro <geovana_goncalves@educadventista.org>
2025-12-02 08:07:33 -0300

    frontend

---
+++ script.js
@@ -0,0 +1,33 @@
+function init(){
+    const navUl = document.querySelector("nav ul")
+
+    const user = JSON.parse(sessionStorage.getItem("user"))
+
+    if(user){
+        navUl.innerHTML += `
+        <li>
+            <a href = "./pages/jogar.html">Jogar</a>
+        </li>
+        <li><h2>Usuário: ${user.name}</h2></li>
+        <li><button id = "logout">Sair</button></li>
+        
+        `
+
+        const logoutButton = document.querySelector("#logout")
+        logoutButton.addEventListener("click", logout)
+        return
+    }
+
+    navUl.innerHTML += `
+    <li>
+        <a href="./pages/login.html">Login</a>
+    </li>
+    `
+}
+
+function logout(){
+    sessionStorage.removeItem("user")
+    window.location.reload()
+}
+
+init()
